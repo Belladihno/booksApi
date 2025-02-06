5882a1471af0c074dee18fc0642550cb1d9fef4b
Abimbola <abimbolaomisakin678@gmail.com>
2025-02-06 01:24:08 +0100

    new update

--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -8,15 +8,15 @@ class APIFeatures {
     const queryObj = { ...this.queryString };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
-
+    
     // advanced filtering
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b{gte|gt|lte|lt}\b/g, (match) => `$${match}`);
-
+    
     this.query = this.query.find(JSON.parse(queryStr));
     return this;
   }
-
+  
   sort() {
     if (this.queryString.sort) {
       const sortBy = this.queryString.sort.split(',').join('');
@@ -26,7 +26,7 @@ class APIFeatures {
     }
     return this;
   }
-
+  
   limitFields() {
     if (this.queryString.fields) {
       const fields = this.queryString.fields.split(',').join(' ');
@@ -36,7 +36,7 @@ class APIFeatures {
     }
     return this;
   }
-
+  
   paginate() {
     const page = this.queryString.page * 1 || 1;
     const limit = this.queryString.limit * 1 || 100;
